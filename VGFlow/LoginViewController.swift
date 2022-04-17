952e8dbb86d56d95879d30d87a148e7c38c83714
FedericoGuidi <federicoguidi196@gmail.com>
2022-04-17 15:16:45 +0200

    Aggiunto bottone Sign in with Apple

---
+++ VGFlow/LoginViewController.swift
@@ -0,0 +1,48 @@
+//
+//  LoginViewController.swift
+//  VGFlow
+//
+//  Created by Federico Guidi on 17/04/22.
+//
+
+import UIKit
+import AuthenticationServices
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var loginProviderStackView: UIStackView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupProviderLoginView()
+    }
+    
+    func setupProviderLoginView() {
+        let authorizationButton = ASAuthorizationAppleIDButton(type: .default, style: .whiteOutline)
+        authorizationButton.addTarget(self, action: #selector(handleAuthorizationAppleIDButtonPress), for: .touchUpInside)
+        self.loginProviderStackView.addArrangedSubview(authorizationButton)
+    }
+    
+    @objc
+    func handleAuthorizationAppleIDButtonPress() {
+        let appleIDProvider = ASAuthorizationAppleIDProvider()
+        let request = appleIDProvider.createRequest()
+        request.requestedScopes = [.fullName, .email]
+        
+        let authorizationController = ASAuthorizationController(authorizationRequests: [request])
+        //authorizationController.delegate = self
+        //authorizationController.presentationContextProvider = self
+        authorizationController.performRequests()
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
